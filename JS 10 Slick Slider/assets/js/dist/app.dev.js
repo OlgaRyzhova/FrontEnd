1420d5d48c5435418b5eab2ebb74f77090ac94df
Olga <miliukovao@gmail.com>
2022-10-01 17:37:07 +0300

    slick slider

--- JS 10 Slick Slider/assets/js/dist/app.dev.js
+++ JS 10 Slick Slider/assets/js/dist/app.dev.js
@@ -5,3 +5,67 @@ $('.multiple-items').slick({
   slidesToShow: 3,
   slidesToScroll: 3
 });
+$(function () {
+  $('.slider_block').slick({
+    // lazyLoad: 'ondemand',
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    dots: true
+  });
+});
+$(function () {
+  $('.products_wrapper').slick({
+    prevArrow: '<button type="button" class="slick-prev"><i class="icon-prev"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="icon-next"></i></button>',
+    slidesToShow: 5,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        arows: true
+      }
+    }, {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        arows: true
+      }
+    }, {
+      breakpoint: 568,
+      settings: {
+        slidesToShow: 1,
+        dots: true,
+        arows: false
+      }
+    }]
+  });
+});
+$(function () {
+  $('.partner_list').slick({
+    prevArrow: '<button type="button" class="slick-prev"><i class="icon-prev"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="icon-next"></i></button>',
+    slidesToShow: 8,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 8,
+        arows: true
+      }
+    }, {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        arows: true
+      }
+    }, {
+      breakpoint: 568,
+      settings: {
+        slidesToShow: 1,
+        dots: true,
+        arows: false
+      }
+    }]
+  });
+});
