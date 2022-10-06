9f3f36179f3f5d3b51f51f8e72ab8a614c8e23f2
Olga <miliukovao@gmail.com>
2022-10-06 23:13:44 +0300

    new project

---
+++ JS 13 Project JS/assets/js/app.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.heading_slider').slick({
+        // lazyLoad: 'ondemand',
+        prevArrow: false,
+        nextArrow: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+    });
+}); 
