fb7f8549244355630ccb17a7d62cb472e83d8d4f
Olga <miliukovao@gmail.com>
2022-10-09 20:02:42 +0300

    logo

--- JS 13 Project JS/assets/js/app.js
+++ JS 13 Project JS/assets/js/app.js
@@ -25,16 +25,33 @@ $(function() {
     });
 }); 
 
+// GALLERY
+
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+})
+
 
 // MAP 
 
 const map = L.map('map').setView([40.665, -73.798], 13);
-
+const img = 
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([40.665, -73.798], {icon:''}).addTo(map)
+let greenIcon = L.icon({
+    iconUrl: 'map_marker.png',
+    // shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [38, 38], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+L.marker([40.665, -73.798], {icon: img}).addTo(map)
     // .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
 
--- JS 13 Project JS/assets/js/dist/app.dev.js
+++ JS 13 Project JS/assets/js/dist/app.dev.js
@@ -22,14 +22,33 @@ $(function () {
     autoplaySpeed: 1500,
     dots: true
   });
+}); // GALLERY
+
+lightbox.option({
+  'resizeDuration': 200,
+  'wrapAround': true
 }); // MAP 
 
 var map = L.map('map').setView([40.665, -73.798], 13);
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+var img = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+var greenIcon = L.icon({
+  iconUrl: 'map_marker.png',
+  // shadowUrl: 'leaf-shadow.png',
+  iconSize: [38, 38],
+  // size of the icon
+  shadowSize: [50, 64],
+  // size of the shadow
+  iconAnchor: [22, 94],
+  // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],
+  // the same for the shadow
+  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+});
 L.marker([40.665, -73.798], {
-  icon: ''
+  icon: img
 }).addTo(map) // .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
 .openPopup(); // const map = L.map('map').setView([40.66, -73.79], 15);
 // let greenIcon = L.icon({
