15c1054a457ec0b8bdfee7e6be3afd3e13e8ce8b
Olga <miliukovao@gmail.com>
2022-10-11 23:12:34 +0300

    map

--- JS 13 Project JS/assets/js/app.js
+++ JS 13 Project JS/assets/js/app.js
@@ -41,48 +41,22 @@ $(function() {
 
 // MAP 
 
-const map = L.map('map').setView([40.665, -73.798], 13);
+const map = L.map('map').setView([40.761, -73.821], 12);
 
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([40.665, -73.798]).addTo(map)
+const circleIcon= L.icon({
+    iconUrl: '/ap_marker.png',
+
+    iconSize:     [109, 109], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 50], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+L.marker([40.761, -73.821], {icon: circleIcon}).addTo(map)
     .bindPopup('Hello my dear tourist')
     .openPopup();
-
-// const map = L.map('map').setView([40.665, -73.798], 13);
-
-// L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-// let greenIcon = L.icon({
-//     iconUrl: 'map_marker.png',
-//     // shadowUrl: 'leaf-shadow.png',
-
-//     iconSize:     [38, 38], // size of the icon
-//     shadowSize:   [50, 64], // size of the shadow
-//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-//     shadowAnchor: [4, 62],  // the same for the shadow
-//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-// });
-// L.marker([40.665, -73.798], {icon: img}).addTo(map)
-//     // .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//     .openPopup();
-
-
-// const map = L.map('map').setView([40.66, -73.79], 15);
-
-// let greenIcon = L.icon({
-//     iconUrl: 'map_marker.png',
-//     // shadowUrl: 'leaf-shadow.png',
-
-//     iconSize:     [38, 38], // size of the icon
-//     shadowSize:   [50, 64], // size of the shadow
-//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-//     shadowAnchor: [4, 62],  // the same for the shadow
-//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-// });
-
-// L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
