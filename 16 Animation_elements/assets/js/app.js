ff7bec4cb252f153f8c10a3a67b80826a4b50f3b
Olga <miliukovao@gmail.com>
2022-12-27 15:27:37 +0200

    update

--- 16 Animation_elements/assets/js/app.js
+++ 16 Animation_elements/assets/js/app.js
@@ -1,9 +1,9 @@
-$(".menu-icon").bind("click", function () {
-  $(".menu-icon").removeClass("paused").addClass("active");
+$(".menu_icon").bind("click", function () {
+  $(".menu_icon").removeClass("paused").addClass("active");
   $(".menu").removeClass("paused").addClass("active");
 });
 
 $(".menu").bind("click", function () {
-  $(".menu-icon").removeClass("active");
+  $(".menu_icon").removeClass("active");
   $(".menu").removeClass("active");
 });
--- 16 Animation_elements/assets/js/dist/app.dev.js
+++ 16 Animation_elements/assets/js/dist/app.dev.js
@@ -1,10 +1,10 @@
 "use strict";
 
-$(".menu-icon").bind("click", function () {
-  $(".menu-icon").removeClass("paused").addClass("active");
+$(".menu_icon").bind("click", function () {
+  $(".menu_icon").removeClass("paused").addClass("active");
   $(".menu").removeClass("paused").addClass("active");
 });
 $(".menu").bind("click", function () {
-  $(".menu-icon").removeClass("active");
+  $(".menu_icon").removeClass("active");
   $(".menu").removeClass("active");
 });
