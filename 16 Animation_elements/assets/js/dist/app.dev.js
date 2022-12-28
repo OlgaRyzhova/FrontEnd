75a7ecc5de5bc73cf30a245243315e3cf6ac4841
Olga <miliukovao@gmail.com>
2022-12-28 21:44:34 +0200

    second block

--- 16 Animation_elements/assets/js/dist/app.dev.js
+++ 16 Animation_elements/assets/js/dist/app.dev.js
@@ -2,9 +2,9 @@
 
 $(".menu_icon").bind("click", function () {
   $(".menu_icon").removeClass("paused").addClass("active");
-  $(".menu").removeClass("paused").addClass("active");
+  $(".menu_navbar").removeClass("paused").addClass("active");
 });
-$(".menu").bind("click", function () {
+$(".menu_navbar").bind("click", function () {
   $(".menu_icon").removeClass("active");
-  $(".menu").removeClass("active");
+  $(".menu_navbar").removeClass("active");
 });
