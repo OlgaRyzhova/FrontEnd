1062b9db2887721b58d587ecb3bd371f56d26304
Olga <miliukovao@gmail.com>
2022-12-28 23:02:21 +0200

    icons

--- 16 Animation_elements/assets/js/app.js
+++ 16 Animation_elements/assets/js/app.js
@@ -1,3 +1,5 @@
+// NAVBAR block
+
 $(".menu_icon").bind("click", function () {
   $(".menu_icon").removeClass("paused").addClass("active");
   $(".menu_navbar").removeClass("paused").addClass("active");
@@ -7,3 +9,12 @@ $(".menu_navbar").bind("click", function () {
   $(".menu_icon").removeClass("active");
   $(".menu_navbar").removeClass("active");
 });
+
+
+// SHARE block
+
+let menu = document.querySelector('.menu')
+let toggle = document.querySelector('.toggle')
+toggle.onclick = function() {
+  menu.classList.toggle('active')
+}
--- 16 Animation_elements/assets/js/dist/app.dev.js
+++ 16 Animation_elements/assets/js/dist/app.dev.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// NAVBAR block
 $(".menu_icon").bind("click", function () {
   $(".menu_icon").removeClass("paused").addClass("active");
   $(".menu_navbar").removeClass("paused").addClass("active");
@@ -7,4 +8,11 @@ $(".menu_icon").bind("click", function () {
 $(".menu_navbar").bind("click", function () {
   $(".menu_icon").removeClass("active");
   $(".menu_navbar").removeClass("active");
-});
+}); // SHARE block
+
+var menu = document.querySelector('.menu');
+var toggle = document.querySelector('.toggle');
+
+toggle.onclick = function () {
+  menu.classList.toggle('active');
+};
