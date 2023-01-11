8bdca50e186bc3bc7e580f0d57cb33189a148a80
Olga Ryzhova <72630568+OlgaRyzhova@users.noreply.github.com>
2023-01-11 17:49:34 +0200

    line

---
+++ 14 Project_HTML&CSS_Animation/assets/js/app.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.slider_block').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4
+        prevArrow: false,
+        nextArrow: false,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        dots: false,
+    });
+}); 
---
+++ 14 Project_HTML&CSS_Animation/assets/js/dist/app.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(function () {
+  $('.slider_block').slick({
+    // lazyLoad: 'ondemand',
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    dots: false
+  });
+});
